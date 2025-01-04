511311c8d213e749a432195a3a8fd76e39f80273
dbuG-Hun7er <dry_lucas@outlook.com>
2025-01-04 01:26:58 -0300

    Portifolio em desenvolvimento

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-// Efeito de animação no fundo com o movimento do mouse
-document.body.addEventListener('mousemove', (e) => {
-    const x = e.clientX / window.innerWidth;
-    const y = e.clientY / window.innerHeight;
-    const z = e.clientY / window.innerHeight;
-
-    document.body.style.background = `linear-gradient(135deg, 
-        rgba(51, 204, 255, ${1 - x}), 5
-        rgba(255, 153, 51, ${1 - y}), 
-        rgba(51, 204, 255, ${1 - z}), 
-        rgba(255, 102, 102, ${x + y + z}))`;
-});
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+// Typewriter Effect
+const texts = [
+    "dbuGHun7ter",
+    "QA",
+    "PENtester",
+    "Apaixonado por BUGS"
+]
+let speed  =100;
+const textElements = document.querySelector(".typewriter-text");
+let textIndex = 0;
+let charcterIndex = 0;
+function typeWriter(){
+    if (charcterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed);
+    }
+    else{
+        setTimeout(eraseText, 1000)
+    }
+}
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText, 50)
+    }
+    else{
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter, 500)
+    }
+}
+window.onload = typeWriter
