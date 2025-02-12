fbe1f271ea84ecb9eb654461ad74a0f9e29e4753
dbuG-Hun7er <dry_lucas@outlook.com>
2025-02-11 21:59:36 -0300

    alteraçoes

---
+++ download.js
@@ -0,0 +1,15 @@
+document.getElementById("downloadBtn").addEventListener("click", function(){
+    // Defina o caminho do arquivo e o nome do arquivo
+    const pastaUrl = "Lucas-QA/meuSite/docs/Lucas_F_Lima_CV.pdf";
+    const pastaName = "Lucas_F_Lima_CV.pdf";
+    // Crie um elemento <a> e defina o atributo de download
+    const a = document.createElement("a");
+    a.href = pastaUrl;
+    a.download = pastaName;
+    // Adicione o elemento <a> ao corpo da página
+    document.body.appendChild(a);
+    // Clique no elemento <a> para iniciar o download
+    a.click();
+    // Remova o elemento <a> do corpo da página
+    document.body.removeChild(a);
+});
